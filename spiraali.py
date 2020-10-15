8fa10c2637a1af37c1dd729eea4d983d7d09feda
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-15 18:21:32 +0300

    done() pääohjelmaan

--- spiraali.py
+++ spiraali.py
@@ -1,5 +1,7 @@
 from turtle import *
 def draw_spiral(color, arcs, r, r_growth, pen):
+    if pen==False:
+        pen=1
     up()
     color(color)
     pensize(pen)
@@ -8,7 +10,9 @@ def draw_spiral(color, arcs, r, r_growth, pen):
         circle(r,90)
         r+=r_growth
     up()
+  
 
-draw_spiral("black", 20, 10, 3)
+#draw_spiral("black", 20, 10, 3)
 draw_spiral("red", 10, 20, 4, 3)
 draw_spiral("blue", 10, -20, -4, 3)
+done()
