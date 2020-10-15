7d877746050e1010f69af2427ec3c8c570c00130
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-15 19:01:50 +0300

    Valmis korjauksilla.

--- spiraali.py
+++ spiraali.py
@@ -1,18 +1,18 @@
 from turtle import *
-def draw_spiral(color, arcs, r, r_growth, pen):
+def draw_spiral(line_color, arcs, r, r_growth, pen=1):
     if pen==False:
         pen=1
     up()
-    color(color)
+    color(line_color)
     pensize(pen)
     down()
-    for i in arcs:
+    for i in range(arcs):
         circle(r,90)
         r+=r_growth
     up()
   
 
-#draw_spiral("black", 20, 10, 3)
+draw_spiral("black", 20, 10, 3)
 draw_spiral("red", 10, 20, 4, 3)
 draw_spiral("blue", 10, -20, -4, 3)
 done()
