77168076ea01f0c1b23291360b7d07b01c19dfeb
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-15 15:57:49 +0300

    Pohja ja draw_spiral alku

--- spiraali.py
+++ spiraali.py
@@ -1,1 +1,13 @@
+from turtle import *
+def draw_spiral(color, arcs, r, r_growth, pen):
+    up()
+    color(color)
+    pensize(pen)
+    down()
+    for i in arcs:
 
+    up()
+
+draw_spiral("black", 20, 10, 3)
+draw_spiral("red", 10, 20, 4, 3)
+draw_spiral("blue", 10, -20, -4, 3)
